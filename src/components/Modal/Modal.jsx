fb242b478e98dceac84a9af24ce3697ce5a31d93
Denis <denis.tolmachev@gmail.com>
2022-07-18 00:24:05 +0300

    add close modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,31 +1,25 @@
 import { Component } from 'react';
-import {ModalBackdrop, ModalImg} from './Modal.styled'
+import { ModalBackdrop, ModalImg } from './Modal.styled';
 
 export class Modal extends Component {
   componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
+    window.addEventListener('keydown', this.closeModal);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
+    window.removeEventListener('keydown', this.closeModal);
   }
 
-  handleKeyDown = e => {
-    if (e.key === 'Escape') {
-      this.props.onClick();
-    }
-  };
-
-  handleBackdropClick = e => {
-    if (e.target === e.currentTarget) {
+  closeModal = e => {
+    if (e.key === 'Escape' || e.target === e.currentTarget) {
       this.props.onClick();
     }
   };
 
   render() {
-    const { onClick, modalImg } = this.props;
+    const { modalImg } = this.props;
     return (
-      <ModalBackdrop onClick={onClick}>
+      <ModalBackdrop onClick={this.closeModal}>
         <div>
           <ModalImg src={modalImg} alt="" />
         </div>
